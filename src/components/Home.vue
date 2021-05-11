f1e85b954e9fd8b662107d6b55c1b506851906b0
Matt Hill <matthill@Matts-MacBook-Pro.local>
2021-05-11 11:03:01 +0100

    home page styling

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,102 @@
 <template>
-  <h1>home</h1>
+<div>
+  <div class = "container" style = "margin-top:20px">
+        <div class = "row d-flex justify-content-center animation_row">
+            <div class = "col-5">
+                <lottie-player src="https://assets8.lottiefiles.com/packages/lf20_v5TxPx.json"  background="transparent"  speed="1.15"  style="width: 3000; height: 300;"  loop  autoplay></lottie-player>
+                <h1 class="ml1">
+                    <span class="text-wrapper">
+                      <span class="line line1"></span>
+                      <span class="letters">PIZZA ROI</span>
+                      <span class="line line2"></span>
+                    </span>
+                  </h1>                  
+            </div>
+        </div>
+
+        <div class = "row d-flex justify-content-center animation_row" style = "padding-top:100px;">
+          <div class = "col-8" data-aos="fade-down" data-aos-delay = "600">
+            <h3 style = "font-size:80px;">
+              The Best Pizza in Ireland
+            </h3>
+        </div>
+     </div>
+    </div>
+
+    <div class = "container" style = "padding-top:40px">
+      <div class = "row d-flex justify-content-center">
+        <div class = "col-8" data-aos="fade-up" data-aos-delay="400" data-aos-duration="800">
+          <p style = "text-align: center;">
+            With a passion for great food, our chefs also offer a selection of delicious hand-made pasta and other main course dishes. If you plan on leaving room for something sweet, we won’t disappoint with one of our tempting home-made desserts. Our wines and beers have been carefully selected to complement our cosmopolitan menu, with a choice to satisfy every palate and pocket. All our pizzas come in a 12-inch square size but for the ultimate food experience combine and share your favourites from the menu on a one-yard or two-yard long pizza. Food this good deserves to be shared!
+            </p>
+      </div>
+    </div>
+    </div>
+
+    <div class = "container" style = "padding-top:40px;">
+      <div class = "row">
+        <div class = "col-7 shadow-lg" style = "background-color:#006491; padding:2.5%; color:#ffffff;">
+          <h4 style = "font-size:45px;">SUPERIOR DOUGH</h4>
+          <p>We make the dough for our Roman-style pizza base in-house every day using the finest grade Italian flour and extra-virgin olive oil. Matured in 3 stages over 72 hours, it is hand tossed, stretched, topped and then stone baked in our custom-built Castelli oven from Rome.</p>
+      </div>
+    </div>
+    </div>
+
+    <div class = "container" style = "padding-top:40px;">
+      <div class = "row d-flex justify-content-end">
+        <div class = "col-7 shadow-lg rellax" style = "background-color:#e31837; padding:2.5%; color:#ffffff;" data-rellax-speed="3">
+          <h4 style = "font-size:45px;">INTERNATIONAL FLAVORS</h4>
+          <p>Our light and crispy Romana pizza bases zing with flavour, with toppings inspired by recipes from around the world. Only fresh, high quality ingredients make it to our kitchen, locally sourced where possible.</p>
+      </div>
+    </div>
+    </div>
+
+    <div class = "container" style = "padding-top:40px;">
+      <div class = "row">
+        <div class = "col-7 shadow-lg rellax" style = "background-color:#006491; padding:2.5%; color:#ffffff;" data-rellax-speed="3">
+          <h4 style = "font-size:45px;">ULTIMATE SHARING EXPERIENCE</h4>
+          <p></p>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-export default {
+// import AOS from 'aos'
+// import anime from 'animejs'
+// import rellax from 'rellax'
 
-}
+//  AOS.init();
+
+//      var textWrapper = document.querySelector('.ml1 .letters');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+// anime.timeline({loop: false})
+//   .add({
+//     targets: '.ml1 .letter',
+//     scale: [0.3,1],
+//     opacity: [0,1],
+//     translateZ: 0,
+//     easing: "easeOutExpo",
+//     duration: 600,
+//     delay: (el, i) => 70 * (i+1)
+//   }).add({
+//     targets: '.ml1 .line',
+//     scaleX: [0,1],
+//     opacity: [0.5,1],
+//     easing: "easeOutExpo",
+//     duration: 700,
+//     offset: '-=875',
+//     delay: (el, i, l) => 80 * (l - i)
+//   })
+  // .add({
+  //   targets: '.ml1',
+  //   opacity: 0,
+  //   duration: 1000,
+  //   easing: "easeOutExpo",
+  //   delay: 1000
+  // });
 </script>
 
 
